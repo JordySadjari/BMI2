06bf15ea9d23fd6714c3bfd56e77e7e14fc4c47e
JordySadjari <jordy_sadjari@hotmail.com>
2016-09-20 15:10:37 +0200

    Bmi Calculator.
    Bmi calculator af versie denk ik.

--- BMI/main.c
+++ BMI/main.c
@@ -1,19 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float calc(float lengte , float weight){
+        float bmi;
+        bmi = weight / (lengte * lengte);
+        return bmi;
+    }
+
 int main()
 {
-    int weight;
-    int lengte;
+    float weight;
+    float lengte;
     char name[100];
+    float bmi;
+
 
     printf( "Hallo zou u uw naam kunnen invullen.\n" );
     scanf( "%s", &name );
+
     printf( "Hallo %s kan je je lengte in meters invullen.\n", name );
-    scanf( "%d", &lengte );
-    printf( "%s kan je je gewicht in kg invullen.\n", weight );
-    scanf( "%d", &weight );
+    scanf( "%f", &lengte );
 
-    return 0;
+    printf( "%s kan je je gewicht in kg invullen.\n", name );
+    scanf( "%f", &weight );
 
+    bmi = calc(lengte , weight);
+
+    if(bmi < 18.5){
+        printf("%s je bent ondergewicht.\n je moet meer gaan eten." , name);
+    }
+
+    if(bmi > 18.5 && bmi < 24.9){
+        printf("%s je hebt een gezond gewicht.\n ga door met waar je mee bezig bent." , name);
+    }
+
+    if(bmi > 25 && bmi < 29.9){
+        printf("%s je hebt geen overgewicht.\n maar je mag niet dikker worden." , name);
+    }
+
+    if(bmi == 30 || bmi > 30 && bmi < 39.9){
+        printf("%s je hebt overgewicht(obesitas).\n verhoogde kans op allerlei aandoeningen\n zoals diabetes, hartaandoeningen en rugklachten.\n je zou 5 tot 10 kg moeten vermageren." , name);
+    }
+
+    if(bmi > 40){
+        printf("%s je bent kapot dick.\n HAHAHAHAHA xD." , name);
+    }
+
+    printf("\n%.2f", weight);
+    printf("\n%.2f", lengte);
+    printf("\n%.2f", bmi);
 }
